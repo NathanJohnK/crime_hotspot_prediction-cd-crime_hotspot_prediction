5efa697e6f3458418ab8696dbbc67e132df26dbe
Nathan Stuttard <nathanstuttard@Nathans-MacBook-Pro.local>
2025-03-27 14:08:36 +0000

    deleted code to redo later

--- fetch_data.py
+++ fetch_data.py
@@ -1,16 +1,8 @@
 import requests
 import pandas as pd
 
-def fetch_crime_data(lat, lng, date):
-    url = f"https://data.police.uk/api/crimes-street/all-crime?lat={lat}&lng={lng}&date={date}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Error fetching data:", response.status_code)
-        return None
+url = f"https://data.police.uk/api/crimes-street/all-crime?lat={lat}&lng={lng}&date={date}"
 
-if __name__ == "__main__":
-    crimes = fetch_crime_data(52.629729, -1.131592, "2024-01")
-    df = pd.DataFrame(crimes)
-    df.to_csv("crime_data.csv", index=False)
+## API request
+#Convert Data to a DataFrame
+## Save the Data as CSV
